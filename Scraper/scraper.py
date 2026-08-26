fd587b6b5b9434e911dc0fac9eb5eb3b29f2752e
MuhammadSalman <salmanshamir9@gmail.com>
2026-08-26 04:22:57 -0700

    fix duplicate pages scrape problem

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,21 +1,24 @@
 # Import the required libraries
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import pandas as pd
 
 
-url = "https://www.flipkart.com/search?q=laptops&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off"
+base_url = "https://www.flipkart.com/search?q=laptops&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off"
 
 
-# launch browser and open target website
+# Launch browser and open target website
 driver = webdriver.Chrome()
-driver.get(url)
+
+wait = WebDriverWait(driver, 15)
+
 time.sleep(3)
-# driver.maximize_window()
 
 
-# Lists to store scrape laptop information
+# Lists to store scraped laptop information
 names = []
 processors = []
 rams = []
@@ -26,110 +29,124 @@ prices = []
 
 
 # Iterate through multiple pages and collect laptop data
-for page in range(2):
-    print(f"Scraping Page {page+1} ")
+for page in range(40):
+
+    print(f"Scraping Page {page + 1}")
+
+    if page==0:
+        url =  base_url
+    else:
+        url = base_url + f"=off&page={page}"
+
+    # open current page
+    driver.get(url)
     
-    product_cards = driver.find_elements(By.CLASS_NAME, value="jIjQ8S")
+    # wait to load the page
+    time.sleep(3)
+   
+    # Wait for product cards
+    wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "jIjQ8S")))
     
-    
-    # Extract laptop specification from each product card
+
+    product_cards = driver.find_elements(By.CLASS_NAME, "jIjQ8S")
+
+
+    # Extract laptop information
     for card in product_cards:
+
+        # Laptop name
         try:
-            name = card.find_element(By.CLASS_NAME, value="RG5Slk").text
+            name = card.find_element(By.CLASS_NAME, "RG5Slk").text
         except:
             name = None
+
         names.append(name)
-       
-       
-        
-        # Handle missing specification to mantain data consistency
+
+
+        # Processor
         try:
-            processor = card.find_element(By.XPATH, value='.//li[@class="DTBslk" and contains(text(), "Processor")]').text
+            processor = card.find_element(By.XPATH, './/li[@class="DTBslk" and contains(text(), "Processor")]').text
         except:
-            processor=None
+            processor = None
+
         processors.append(processor)
-        
-        
-        
+
+
+        # RAM
         try:
-            ram = card.find_element(By.XPATH, value='.//li[@class="DTBslk" and contains(text(), "RAM")]').text
+            ram = card.find_element(By.XPATH, './/li[@class="DTBslk" and contains(text(), "RAM")]').text
         except:
             ram = None
+
         rams.append(ram)
-        
-        
-        
+
+
+        # SSD
         try:
-            ssd = card.find_element(By.XPATH, value='.//li[@class="DTBslk" and contains(text(), "SSD")]').text
+            ssd = card.find_element(By.XPATH, './/li[@class="DTBslk" and contains(text(), "SSD")]').text
         except:
             ssd = None
+
         ssds.append(ssd)
-       
-        
-        
+
+
+        # System
         try:
-            system = card.find_element(By.XPATH, value='.//li[@class="DTBslk" and contains(text(), "System")]').text
+            system = card.find_element(By.XPATH, './/li[@class="DTBslk" and contains(text(), "System")]').text
         except:
             system = None
-        systems.append(system)     
-       
-        
-        
+
+        systems.append(system)
+
+
+        # Display
         try:
-            display = card.find_element(By.XPATH, value='.//li[@class="DTBslk" and contains(text(), "Display")]').text
+            display = card.find_element(By.XPATH, './/li[@class="DTBslk" and contains(text(), "Display")]').text
         except:
             display = None
+
         displays.append(display)
-        
-        
-        
+
+
+        # Price
         try:
-            price = card.find_element(By.CSS_SELECTOR, value=".hZ3P6w.DeU9vF").text
+            price = card.find_element(By.CSS_SELECTOR, ".hZ3P6w.DeU9vF").text
         except:
             price = None
+
         prices.append(price)
-        
-      
     
-    # Navigate to next the page
-    next_button = driver.find_element(By.CLASS_NAME, value="jgg0SZ")
-    
-    
-    # Scroll to button
-    driver.execute_script("arguments[0].scrollIntoView();", next_button)
     time.sleep(1)
 
-    # Click safely
-    driver.execute_script("arguments[0].click();", next_button)
-    time.sleep(5)
 
 
-# Store the extracted data lists in a dictionary form
+
+
+# Store extracted data
 data = {
-    "Name":names,
-    "Price":prices,
-    "Ram":rams,
-    "System":systems,
-    "SSD":ssds,
-    "Processor":processors,
-    "Display":displays
+    "Name": names,
+    "Price": prices,
+    "Ram": rams,
+    "System": systems,
+    "SSD": ssds,
+    "Processor": processors,
+    "Display": displays
 }
 
 
-
-# Make a dataframe from a dictionary 
+# Make dataframe
 df = pd.DataFrame(data)
 
 
+# Save dataframe
+df.to_csv(
+    "Data/raw_data.csv",
+    index=False
+)
 
-# Save the dataframe as a csv file
-df.to_csv("Data/raw_data.csv", index=False)
 
-
-# Close the browser
+# Close browser
 driver.quit()
 
+
 print(f"Total Records Collected : {len(df)}")
-        
-        
-        
